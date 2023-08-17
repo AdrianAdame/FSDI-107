c36c9d60bebec6392340a1b46ba84aca89b5d98c
Adrian Adame <adrian_mustang31@hotmail.com>
2023-08-16 19:51:09 -0700

    Added creation form in admin

--- marketla/src/components/Footer/index.jsx
+++ marketla/src/components/Footer/index.jsx
@@ -3,16 +3,36 @@ import "./footer.css"
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return(
-        <footer class="py-3">
-            <ul class="nav justify-content-center border-bottom pb-3">
-            <li class="nav-item"><Link href="#" class="nav-link px-2 text-muted" to="/">Home</Link></li>
-            <li class="nav-item"><Link href="#" class="nav-link px-2 text-muted" to="/cart">Your Cart</Link></li>
-            <li class="nav-item"><Link href="#" class="nav-link px-2 text-muted" to="/about">About</Link></li>
+    return (
+        <footer className="py-3">
+            <ul className="nav justify-content-center border-bottom pb-3">
+                <li className="nav-item">
+                    <Link href="#" className="nav-link px-2 text-muted" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        href="#"
+                        className="nav-link px-2 text-muted"
+                        to="/cart"
+                    >
+                        Your Cart
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        href="#"
+                        className="nav-link px-2 text-muted"
+                        to="/about"
+                    >
+                        About
+                    </Link>
+                </li>
             </ul>
-            <p class="text-center text-muted">© 2023 Marketla, Inc</p>
+            <p className="text-center text-muted">© 2023 Marketla, Inc</p>
         </footer>
-    )
+    );
 }
 
 export default Footer;
--- marketla/src/pages/Admin/index.jsx
+++ marketla/src/pages/Admin/index.jsx
@@ -1,7 +1,104 @@
+import { useState } from "react";
 import "./admin.css";
 
 const Admin = () => {
-    return <div className="admin page">This is an admin page</div>;
+    const [product, setProduct] = useState({
+        title: "",
+        category: "",
+        image: "",
+        price: "",
+    });
+
+    const handleInputChange = (e) => {
+        const id = e.target.id;
+        const val = e.target.value;
+
+        let copy = { ...product }; //Spread operator
+        copy[id] = val;
+
+        setProduct(copy);
+    };
+
+    const saveProduct = () => {
+        console.log(product);
+
+        clearForm();
+    };
+
+    const clearForm = () => {
+        setProduct({ title: "", category: "", image: "", price: "" });
+    };
+
+    return (
+        <div className="admin page">
+            <h1>Store Management</h1>
+
+            <div className="parent-container">
+                <h3>Register Product</h3>
+                <div>
+                    <label htmlFor="title" className="form-label">
+                        Title
+                    </label>
+                    <input
+                        value={product.title}
+                        onChange={handleInputChange}
+                        type="text"
+                        name="Title"
+                        id="title"
+                        className="form-control"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="category" className="form-label">
+                        Category
+                    </label>
+                    <input
+                        value={product.category}
+                        onChange={handleInputChange}
+                        type="text"
+                        name="Category"
+                        id="category"
+                        className="form-control"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="image" className="form-label">
+                        Image
+                    </label>
+                    <input
+                        value={product.image}
+                        onChange={handleInputChange}
+                        type="text"
+                        name="Image"
+                        id="image"
+                        className="form-control"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="price" className="form-label">
+                        Price
+                    </label>
+                    <input
+                        value={product.price}
+                        onChange={handleInputChange}
+                        type="number"
+                        name="Price"
+                        id="price"
+                        className="form-control"
+                    />
+                </div>
+
+                <div>
+                    <button
+                        onClick={() => saveProduct()}
+                        className="btn btn-dark"
+                    >
+                        Save Product
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Admin;
